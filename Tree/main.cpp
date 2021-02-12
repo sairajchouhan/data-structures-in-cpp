4ee9065156c44941323c89d470c7ab3082fddd4e
sairaj <sairaj2119@gmail.com>
2021-02-12 20:16:41 +0530

    add itterative methods of preorder inorder and postorder

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 using namespace std;
 
 class Node
@@ -35,6 +36,9 @@ public:
   void inOrder(Node *p);
   void levelOrder(Node *p);
   void height(Node *p);
+  void preOrderIter(Node *p);
+  void inOrderIter(Node *p);
+  void postOrderIter(Node *p);
 };
 
 void Tree::createTree()
@@ -118,18 +122,102 @@ void Tree::levelOrder(Node *p)
   }
 }
 
+void Tree::preOrderIter(Node *p)
+{
+  stack<Node *> st;
+  while (!st.empty() || p != nullptr)
+  {
+    if (p != nullptr)
+    {
+      st.push(p);
+      cout << p->data << " ";
+      p = p->lchild;
+    }
+    else
+    {
+      p = st.top();
+      st.pop();
+      p = p->rchild;
+    }
+  }
+}
+
+void Tree::inOrderIter(Node *p)
+{
+  stack<Node *> st;
+  while (!st.empty() || p != nullptr)
+  {
+    if (p != nullptr)
+    {
+      st.push(p);
+      p = p->lchild;
+    }
+    else
+    {
+      p = st.top();
+      st.pop();
+      cout << p->data << " ";
+      p = p->rchild;
+    }
+  }
+}
+
+void Tree::postOrderIter(Node *p)
+{
+  stack<long int> st;
+  long long int temp{0};
+
+  while (!st.empty() || p != nullptr)
+  {
+    if (p != nullptr)
+    {
+      st.push((long int)p);
+      p = p->lchild;
+    }
+    else
+    {
+      temp = st.top();
+      st.pop();
+      if (temp > 0)
+      {
+        st.push(0 - temp);
+        p = ((Node *)temp)->rchild;
+      }
+      else
+      {
+        cout << ((Node *)(0 - temp))->data << " ";
+        p = nullptr;
+      }
+    }
+  }
+}
+
 int main()
 {
 
   Tree t;
   t.createTree();
+
+  cout << "PreOrder traversal using reccursion: ";
   t.preOrder(t.root);
   cout << endl;
+  cout << "PreOrder traversal using iteration: ";
+  t.preOrderIter(t.root);
+  cout << "\n----------------------------------------------------------" << endl;
+
+  cout << "InOrder traversal using reccursion: ";
   t.inOrder(t.root);
   cout << endl;
+  cout << "InOrder traversal using iteration: ";
+  t.inOrderIter(t.root);
+  cout << "\n----------------------------------------------------------" << endl;
+
+  cout << "PostOrder traversal using reccursion: ";
   t.postOrder(t.root);
   cout << endl;
-  t.levelOrder(t.root);
+  cout << "PostOrder traversal using iteration: ";
+  t.postOrderIter(t.root);
+  cout << "\n----------------------------------------------------------" << endl;
 
   return 0;
 }
